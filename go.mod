84268b156fdf6fc0b3950a7d15231f0d6615a81c
smalldog124 <panumars124@gmail.com>
2020-01-09 08:24:14 +0700

    [Edited]:ปรับเปลี่ยนใช้ library ของ gocll/colly

--- go.mod
+++ go.mod
@@ -3,7 +3,20 @@ module crawler
 go 1.13
 
 require (
+	github.com/PuerkitoBio/goquery v1.5.0 // indirect
+	github.com/antchfx/htmlquery v1.2.1 // indirect
+	github.com/antchfx/xmlquery v1.2.2 // indirect
+	github.com/antchfx/xpath v1.1.3 // indirect
 	github.com/gin-gonic/gin v1.5.0
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0
+	github.com/golang/groupcache v0.0.0-20191227052852-215e87163ea7 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/stretchr/testify v1.4.0
+	github.com/temoto/robotstxt v1.1.1 // indirect
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
--- main.go
+++ main.go
@@ -2,31 +2,14 @@ package main
 
 import (
 	"crawler/db"
-	"crawler/find"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"time"
 
 	"github.com/globalsign/mgo"
+	"github.com/gocolly/colly"
 )
 
 func main() {
-	resp, err := http.Get("http://www.e-service.nlt.go.th/ISBNReq/ListSearchPub?keywordTypeKey=1")
-	if err != nil {
-		log.Println("call fail", err)
-		return
-	}
-	body, _ := ioutil.ReadAll(resp.Body)
-	table := find.GetTableData(body)
-	books := find.BooksDetail(table)
-	// err = ioutil.WriteFile("books.html", body, 0644)
-	// if err != nil {
-	// 	fmt.Println("writeFile fail")
-	// }
-
-	fmt.Printf("books !!!\n %v", books)
 	DBConnection, err := mgo.Dial(db.MongoEndPoint)
 	if err != nil {
 		log.Fatalf("Cannot connect database %s\n", err.Error())
@@ -35,16 +18,27 @@ func main() {
 		DBConnection: DBConnection,
 		Database:     db.Database,
 	}
-	var booksDetail []db.Book
-	for _, book := range books {
-		b := db.Book{
-			Name:      book[0],
-			Writer:    book[1],
-			Publisher: book[2],
-			Updatated: time.Now(),
-		}
-		booksDetail = append(booksDetail, b)
+	var detail []string
+	c := colly.NewCollector()
+
+	c.OnHTML("input[value]", func(e *colly.HTMLElement) {
+		detail = append(detail, e.Attr("value"))
+
+	})
+	c.Visit("http://e-service.nlt.go.th/ISBNReq/Detail/205789")
+	var book db.Book
+	book.Name = detail[0]
+	book.Writer = detail[1]
+	book.Translator = detail[2]
+	book.EditionNote = detail[4]
+	book.PrintYear = detail[6]
+	book.NumberOfPage = detail[7]
+	book.ISBNumber = detail[8]
+	book.Publisher = detail[9]
+
+	if err = bookMongo.SaveBooks(book); err != nil {
+		log.Println("data base err: ", err)
 	}
-	log.Println(bookMongo.SaveBooks(booksDetail))
+
 	fmt.Println("Yes !!!")
 }
