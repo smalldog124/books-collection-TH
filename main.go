dbccae7d2bc03d60f249d53ff1568a4eec6f0046
smalldog124 <panumars124@gmail.com>
2020-01-20 15:43:28 +0700

    [Edited]: แก้ไขจากใช้ goroutine เป็น go chael ที่ไฟล์ main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"crawler/db"
 	"fmt"
 	"log"
-	"sync"
+	_ "net/http/pprof"
 	"time"
 
 	"github.com/globalsign/mgo"
@@ -21,30 +21,31 @@ func main() {
 		Database:     db.Database,
 	}
 
-	var detail []db.Book
-	var wg sync.WaitGroup
 	c := colly.NewCollector()
-	for round := 0; round <= 200; round += 10 {
+	firstReq := time.Now()
+	booksChan := make(chan []db.Book)
+	for round := 0; round < 20; round += 10 {
 		stratTime := time.Now()
-		wg.Add(1)
-		go visitWebsite(c, round, &wg, detail)
-		wg.Wait()
+
+		go visitWebsite(c, round, booksChan)
+		detail := <-booksChan
 
 		if err = bookMongo.SaveBooks(detail); err != nil {
 			log.Println("data base err: ", err)
 		}
-		fmt.Printf("s: %v book: %d\n", time.Since(stratTime), round)
+		fmt.Printf("s: %v book: %d\n", time.Since(stratTime), round+10)
 	}
-	fmt.Println("Yes !!!")
+	fmt.Println("Yes !!!", time.Since(firstReq))
 }
 
-func visitWebsite(c *colly.Collector, round int, wg *sync.WaitGroup, detail []db.Book) {
-	defer wg.Done()
-	for i := 1; i < 10; i++ {
-		index := (round * 10) + i
+func visitWebsite(c *colly.Collector, round int, booksChan chan []db.Book) {
+	var detail []db.Book
+	for i := 1; i <= 10; i++ {
+		index := round + i
 		b := crawler(c, index)
 		detail = append(detail, b)
 	}
+	booksChan <- detail
 }
 
 func crawler(c *colly.Collector, index int) db.Book {
